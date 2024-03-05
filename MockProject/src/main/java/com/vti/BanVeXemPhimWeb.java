52cf5a52ded3ac402d85963c7b10ecc959a551d9
Username <ghieu0123@gmail.com>
2024-03-05 15:53:55 +0700

    day file

---
+++ MockProject/src/main/java/com/vti/BanVeXemPhimWeb.java
@@ -0,0 +1,12 @@
+package com.vti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanVeXemPhimWeb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanVeXemPhimWeb.class, args);
+	}
+}
